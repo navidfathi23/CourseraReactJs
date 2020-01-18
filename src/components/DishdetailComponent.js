66edf328c95ac97a0731ccc7cc2fa9a19d6eb3d0
Navidfathi98 <navidfathi98@gmail.com>
2020-01-18 16:48:46 +0330

    Fetch and server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,13 +4,13 @@ import { Media , Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import {Errors , LocalForm , Control} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderDish({ dish }) {
         if (dish != null)
             return(
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
